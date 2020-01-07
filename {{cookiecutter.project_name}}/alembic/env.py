e282feffa67965d0b1c17658fb3b15e209f6d616
NoThanks <kanonxmm@163.com>
2020-01-07 11:42:35 +0800

    fix related path error and alembic error
    url = config.get_main_option("sqlalchemy.url")
    if there are % signs in the url, this will cause an error.
    alembic invalid interpolation syntax

--- {{cookiecutter.project_name}}/alembic/env.py
+++ {{cookiecutter.project_name}}/alembic/env.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) +"/../")
+
 import importlib
 from logging.config import fileConfig
 
@@ -58,7 +62,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option("sqlalchemy.url").replace('%', '%%')
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True
     )
